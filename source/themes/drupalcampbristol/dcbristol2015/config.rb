48428db0eecda3d627d3b916f1a0ab0d75cd1a0d
Oliver Davies <oliver@oliverdavies.co.uk>
2015-03-15 21:06:16 +0000

    Added SASS and Compass files

---
+++ source/themes/drupalcampbristol/dcbristol2015/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+require 'sass-globbing'
+require 'bootstrap-sass'
+
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'assets/scss'
+images_dir = 'assets/img'
+javascripts_dir = 'assets/js'
+
+output_style = :compressed
+relative_assets = true
+line_comments = false
